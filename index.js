18d171c40461e88cd9c1c8caf4acaf75ad0c9214
Brayden Aimar <baimar97@hotmail.com>
2017-03-12 14:49:08 -0700

    Added dev and deployment window modes
    If the program is launched on the host 'BRAYDENS-LAPTOP' the window will
    be launched and dev tools will also be launched.
    
    If the program is launched on any other device, the window will be
    launched in kiosk mode.

--- index.js
+++ index.js
@@ -2,54 +2,96 @@
 
 // This script is the first to be ran and loads the HTML file that then loads the appropriate JS and CSS files.
 
-const electron = require('electron')
-const {app, BrowserWindow, ipcMain, ipcRenderer} = electron
+const electron = require('electron');
+const { app, BrowserWindow, ipcMain } = electron;
 // const ipcMain = require('electron').ipcMain;
-const os = require('os')
+const os = require('os');
 
-console.log("running index.js")
-console.log("electron", electron)
+console.log('running index.js');
+console.log('electron', electron);
 
-app.on('browser-window-created',function (e, window) {
-	window.setMenu(null)
-})
+console.log(`Host Device: '${os.hostname()}'`);
+
+app.on('browser-window-created', function (e, window) {
+	window.setMenu(null);
+
+});
 
 app.on('ready', () => {
-	let win = new BrowserWindow({
-		// show: false,
-		width: 1650,
-		height: 950,
-		// fullscreen: true,
-		// kiosk: true,
-		// frame: false,
-		// backgroundColor: '#eaedf4',
-		backgroundThrottling: false,
-		// webPreferences: {
-		// 	nodeIntegration: false
-		// }
-		icon: __dirname + '/icon.ico'
-	})
+	let win = null;
 
-	win.loadURL(`file://${__dirname}/main.html`)
-	win.webContents.openDevTools()
+	// If running this on braydens laptop, open it in development mode.
+	if (os.hostname() === 'BRAYDENS-LAPTOP') {
+
+		console.log('Opening in development mode.');
+
+		win = new BrowserWindow({
+			// show: false,
+			width: 1650,
+			height: 950,
+			// fullscreen: true,
+			// kiosk: true,
+			// frame: false,
+			// backgroundColor: '#eaedf4',
+			backgroundThrottling: false,
+			// webPreferences: {
+			// 	nodeIntegration: false
+			// }
+			icon: __dirname + '/icon.ico'
+		});
+
+	} else {
+
+		console.log('Opening in deployment mode.');
+
+		win = new BrowserWindow({
+			// show: false,
+			// width: 1650,
+			// height: 950,
+			// fullscreen: true,
+			kiosk: true,
+			// frame: false,
+			// backgroundColor: '#eaedf4',
+			backgroundThrottling: false,
+			// webPreferences: {
+			// 	nodeIntegration: false
+			// }
+			icon: __dirname + '/icon.ico'
+		});
+
+	}
+
+	win.loadURL(`file://${__dirname}/main.html`);
+
+	if (os.hostname() === 'BRAYDENS-LAPTOP') win.webContents.openDevTools();
 
 	win.webContents.on('did-finish-load', function () {
-		console.log('Got \'did-finish-load\' event.')
-	})
+
+		console.log('Got \'did-finish-load\' event.');
+
+	});
 
 	// Called after all widgets initBody() functions and after initial visibility has been set and sidebar buttons have been created.
 	ipcMain.on('all-widgets-loaded', function() {
-		win.show()
-		console.log("Got ipcMain event: 'all-widgets-loaded'.\n  ...showing window.")
-	})
+
+		win.show();
+
+		console.log('Got ipcMain event: \'all-widgets-loaded\'.\n  ...showing window.');
+
+	});
 
 	ipcMain.on('open-dev-tools', function() {
-		win.webContents.openDevTools()
-		console.log("Got ipcMain event: 'open-dev-tools'.")
-	})
+
+		win.webContents.openDevTools();
+
+		console.log('Got ipcMain event: \'open-dev-tools\'.');
+
+	});
 
 	win.on('close', function () {
-		// amplify.publish('/connection-widget/spjs-send', 'exit');
-		win = null
-	})
+
+		win = null;
+
+	});
+
 })
